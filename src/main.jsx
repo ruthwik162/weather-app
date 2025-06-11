b851c7744c39c5fae064a3cb3373426a7c04184f
M.Nagaruthwik <nagaruthwikmerugu162@gmail.com>
2025-06-11 16:21:39 +0530

    main changed for deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/weather-app/'>
+  <BrowserRouter basename="/weather-app/"> {/* ✅ Matches Vite base */}
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/weather-app/",
-  plugins: [react(),tailwindcss()],
+  base: '/weather-app/', // ✅ IMPORTANT
+  plugins: [react()],
 })
