6097d563b6c7b30de246569a8f92c64fc6ae8f61
M.Nagaruthwik <nagaruthwikmerugu162@gmail.com>
2025-07-04 22:49:35 +0530

    Icon Updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/weather-app/', // ✅ IMPORTANT
+   // ✅ IMPORTANT
   plugins: [react(), tailwindcss()],
 })
